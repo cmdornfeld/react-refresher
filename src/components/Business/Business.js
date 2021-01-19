1749fb14df18d16754f47a704ed34520bb6911e0
Chad Dornfeld <cmdornfeld@gmail.com>
2021-01-19 11:06:46 -0600

    completed steps 7-10 of part II

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -7,32 +7,32 @@ class Business extends React.Component {
         return (
             <div className="Business">
                 <div className="image-container">
-                    <img src={business.imageSrc} alt={business.name}/>
+                    <img src={this.props.business.imageSrc} alt={this.props.business.name}/>
                 </div>
                 <h2>
-                    {business.name}
+                    {this.props.business.name}
                 </h2>
                 <div className="Business-information">
                     <div className="Business-address">
                         <p>
-                            {business.address}
+                            {this.props.business.address}
                         </p>
                         <p>
-                            {business.city}
+                            {this.props.business.city}
                         </p>
                         <p>
-                            {business.state} {business.zipCode}
+                            {this.props.business.state} {this.props.business.zipCode}
                         </p>
                     </div>
                     <div className="Business-reviews">
                         <h3>
-                            {business.category}
+                            {this.props.business.category}
                         </h3>
                         <h3 className="rating">
-                            {business.rating} stars
+                            {this.props.business.rating} stars
                         </h3>
                         <p>
-                            {business.reviewCount} reviews
+                            {this.props.business.reviewCount} reviews
                         </p>
                     </div>
                 </div>
