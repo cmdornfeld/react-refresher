006da0181c64f3b1c454d4151e329984f0721022
Chad Dornfeld <cmdornfeld@gmail.com>
2021-01-19 15:29:21 -0600

    completed steps 14-19

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -15,11 +15,15 @@ class SearchBar extends React.Component {
             location: '',
             sortBy: 'best_match'
         };
+
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+
         this.sortByOptions = {
             'Best Match': 'best_match',
             'Highest Rated': 'rating',
             'Most Reviewed': 'review_count'
-          };
+        };
     }
 
     getSortByClass(sortByOption){
@@ -67,8 +71,14 @@ class SearchBar extends React.Component {
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input 
+                        onChange={this.handleTermChange}
+                        placeholder="Search Businesses" 
+                    />
+                    <input 
+                        onChange={this.handleLocationChange}
+                        placeholder="Where?"
+                    />
                 </div>
                 <div className="SearchBar-submit">
                     {/* eslint-disable-next-line */}
