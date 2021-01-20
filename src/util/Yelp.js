832c097cd55f6ce68d1d009d64509c37671a573b
Chad Dornfeld <cmdornfeld@gmail.com>
2021-01-20 15:45:27 -0600

    updated map syntax in  Yelp.js

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -4,7 +4,8 @@ const Yelp = {
     search(term, location, sortBy){
         return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {headers: {Authorization: `Bearer ${apiKey}`}})
         .then(response => {
-            return response.json();})
+            return response.json();
+        })
         .then(jsonResponse => {
             if (jsonResponse.businesses){
                 return jsonResponse.businesses.map(business => ({
@@ -22,8 +23,7 @@ const Yelp = {
                 );
             }
         });
-    },
-
+    }
 }
 
 export default Yelp;
