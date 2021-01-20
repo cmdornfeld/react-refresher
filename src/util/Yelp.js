a29264c8ea1a3bd14858dbc44e8ca51354fd37bb
Chad Dornfeld <cmdornfeld@gmail.com>
2021-01-20 13:26:37 -0600

    completed steps 1-10 of part IV

---
+++ src/util/Yelp.js
@@ -0,0 +1,8 @@
+const apiKey = 'gO0gZQUZmRLdOoZ0hFJJPPDVK_mdN1i1plDTPuA-byk9wEhtcT5KAtrPz5tAKq0wOqsMU-jVCnPYcpIwtdxCVWbefNPifoFlLt-KHCYMoLJ3lifEE8izjwx0yYIIYHYx';
+
+const Yelp = {
+    search(term, location, sortBy){
+        return fetch()
+    },
+
+}
